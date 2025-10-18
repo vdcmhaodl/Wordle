5386f8de72c6c7057ab210745753a53809469d38
HaoVoxx <84260715+HaoVo123-hihi@users.noreply.github.com>
2025-10-18 09:22:09 +0700

    change the main file; update FE using pygame having rows, cols, keyboard, color; update dataset wordlist

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from backend import WordleGame
-
+from frontend import WordleFrontend
 def load_word_list():
     with open("words.txt") as f:
-        return [line.strip() for line in f.readlines()]
+        return [line.strip().upper() for line in f.readlines()]
 def main():
+    # word_list = load_word_list()
+    # game = WordleGame(word_list)
+    # print("Welcome to Wordle!")
+    # while not game.is_game_over():
+    #     guess = input(f"You have {game.attempts} attempts left. Enter your guess: ").strip().upper()
+    #     try:
+    #         feedback = game.make_guess(guess)
+    #         print("Feedback:", feedback)
+    #     except ValueError as e:
+    #         print(e)
+    # if game.guesses and game.guesses[-1] == game.get_secret_word():
+    #     print("Congratulations! You've guessed the word:", game.get_secret_word())
+    # else:
+    #     print("Game over! The secret word was:", game.get_secret_word())
     word_list = load_word_list()
-    game = WordleGame(word_list)
-    print("Welcome to Wordle!")
-    while not game.is_game_over():
-        guess = input(f"You have {game.attempts} attempts left. Enter your guess: ").strip().upper()
-        try:
-            feedback = game.make_guess(guess)
-            print("Feedback:", feedback)
-        except ValueError as e:
-            print(e)
-    if game.guesses and game.guesses[-1] == game.get_secret_word():
-        print("Congratulations! You've guessed the word:", game.get_secret_word())
-    else:
-        print("Game over! The secret word was:", game.get_secret_word())
-
+    frontend = WordleFrontend(word_list)
+    frontend.run()
 if __name__ == "__main__":
     main()
